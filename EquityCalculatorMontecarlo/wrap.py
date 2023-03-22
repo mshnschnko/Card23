2b5718fdde597da1000012cb418568d1f54c2d82
mshnschnko <misha321654987i@gmail.com>
2023-03-22 18:34:04 +0300

    overlay

--- EquityCalculatorMontecarlo/wrap.py
+++ EquityCalculatorMontecarlo/wrap.py
@@ -1,10 +1,14 @@
 import cppimport
 import time
+import tkinter as tk
+from overlay import Window
 
 mc = cppimport.imp("Scoring")
 
 
 def _run(my_cards, cards_on_table, players):
+    if len(cards_on_table) == 0:
+        cards_on_table = {'\0'}
     equity = mc.montecarlo(my_cards, cards_on_table, players, 10000)
     # print(equity)
     return equity
@@ -25,10 +29,15 @@ def avg_eq(my_cards, cards_on_table, players):
 
 
 if __name__ == "__main__":
-    tic = time.perf_counter()
+    # tic = time.perf_counter()
     my_cards = {'3H', '3S'}
     cards_on_table = {'8S', '4S', 'QH', '8C', '4H'}
     players = 2
-    avg_eq(my_cards, cards_on_table, players)
+    eq = _run(my_cards, cards_on_table, players)
+    win = Window()
+    label = tk.Label(win.root, text=f"Equity =  {str(eq)}%")
+    label.pack()
+    Window.launch()
+    # avg_eq(my_cards, cards_on_table, players)
     # _run(my_cards, cards_on_table, players)
-    print(f"общее время: {time.perf_counter() - tic:0.4f}")
+    # print(f"общее время: {time.perf_counter() - tic:0.4f}")
